61c01907734752517c50f04c306346496182bd3e
Matt <hurley.matt@proton.me>
2026-02-03 20:30:47 -0500

    Add gitignore file

--- critter_ritter/pages/models.py
+++ critter_ritter/pages/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Animal(models.Model):
+    name = models.CharField()
+    bio = models.TextField()
+    date_added = models.DateField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
